c436549a023dba4b03ffba366834fd1edd4960c3
edo-vi <dntfp1@gmail.com>
2017-10-02 01:15:55 +0200

    fixed typo and added some tests to check if digits() raises exceptions

--- hackr/tests/test_generator.py
+++ hackr/tests/test_generator.py
@@ -2,9 +2,16 @@ import pytest
 import hackr
 
 
-def test_digit():
+def test_digits():
     assert len(hackr.generator.digits(3, 0)) == 0
     assert len(hackr.generator.digits(3, 4)) == 4
+    assert len(hackr.generator.digits(3, -6)) == 0  # should it raise an exception?
     assert all(x <= 3 for x in hackr.generator.digits(3, 100))
     assert all(x == 0 for x in hackr.generator.digits(0, 100))
+    with pytest.raises(Exception):
+        hackr.generator.digits('3', 4)
+    with pytest.raises(Exception):
+        hackr.generator.digits(3, '4')
+    with pytest.raises(Exception):
+        hackr.generator.digits(-1, 4)
 
