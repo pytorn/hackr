ff827445df8a2d4275e4d8f1e7732c81f60e53b4
Sujanani <ansujanani@hotmail.com>
2018-10-02 21:29:43 +0530

    Added reqs, formatted functions, added to init

--- hackr/iptools.py
+++ hackr/iptools.py
@@ -6,7 +6,7 @@ import sys
 def getLiveIP():
 	try:
 		res = BeautifulSoup(requests.get('https://www.whatsmyip.com').text, 'lxml')
-		return res.find_all('p')[0].getText()
+		return str(res.find_all('p')[0].getText())
 	except Exception as _:
 		print 'Could not get live IP address'
 
@@ -14,7 +14,7 @@ def getPrivateIP():
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		s.connect(('8.8.8.8', 80))
-		print 'Local IP on machine: ', s.getsockname()[0]
+		return s.getsockname()[0]
 		s.close()
 	except Exception as _:
 		print 'Could not get local IP Address'
