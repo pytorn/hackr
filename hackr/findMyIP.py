9d6f484ca333a82d46540c8e566528dc4917cb66
Sujanani <ansujanani@hotmail.com>
2018-10-02 12:10:09 +0530

    Made functions

--- hackr/findMyIP.py
+++ hackr/findMyIP.py
@@ -3,19 +3,21 @@ import requests
 import socket
 import sys
 
-try:
-	res = BeautifulSoup(requests.get('https://www.whatsmyip.com').text, 'lxml')
-	print 'Live IP addr: ', res.find_all('p')[0].getText()
-except Exception as _:
-	print 'Could not get live IP address'
+def getLiveIP():
+	try:
+		res = BeautifulSoup(requests.get('https://www.whatsmyip.com').text, 'lxml')
+		return res.find_all('p')[0].getText()
+	except Exception as _:
+		print 'Could not get live IP address'
 
-try:
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(('8.8.8.8', 80))
-	print 'Local IP on machine: ', s.getsockname()[0]
-	s.close()
-except Exception as _:
-	print 'Could not get local IP Address'
+def getPrivateIP():
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.connect(('8.8.8.8', 80))
+		print 'Local IP on machine: ', s.getsockname()[0]
+		s.close()
+	except Exception as _:
+		print 'Could not get local IP Address'
 
-sys.exit(0)
+
 
