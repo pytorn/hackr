029c700d906706a7c6cc7901a2ed1c2f341e5419
Iordanis Fostiropoulos <danny.fostiropoulos@gmail.com>
2018-10-28 17:03:57 -0700

    Added cryptocurrency conversion

---
+++ hackr/converter.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+"""
+
+Provides Pricing information on a crypto pair or currency pair, such as USD, BTC, ETH EUR
+
+"""
+def cryptoPricePair(coinA, coinB):
+	try:
+        res = json.loads(requests.get('https://min-api.cryptocompare.com/data/price?fsym=%s&tsyms=%s'%(coinA.upper(),coinB.upper())).text)
+        return str(res[coinB.upper()])
+	except Exception as _:
+		print 'Could not get live IP address'
